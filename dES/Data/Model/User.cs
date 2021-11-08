a64e4a1a1ae3dcc6189181154961dcdd52a7baa6
Karolis2011 <Karolis2011@users.noreply.github.com>
2021-11-08 14:30:49 +0200

    Initial commit

---
+++ dES/Data/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace dES.Data.Model
+{
+    // Add profile data for application users by adding properties to the dESUser class
+    public class User : IdentityUser
+    {
+    }
+}
---
+++ dES/Data/dESContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using dES.Data.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace dES.Data
+{
+    public class dESContext : IdentityDbContext<User>
+    {
+        public dESContext(DbContextOptions<dESContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("Server=localhost;User=des;Password=;Database=des_dev", new MariaDbServerVersion(new Version(10, 6, 3)));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
